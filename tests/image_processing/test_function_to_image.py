c37fde333508ab5d48afe3e099a6912cdc058755
Aurélien Coussat <aurelien.coussat@uj.edu.pl>
2024-09-18 17:22:57 +0200

    Test function_to_image using pytest

--- tests/image_processing/test_function_to_image.py
+++ tests/image_processing/test_function_to_image.py
@@ -1,31 +1,18 @@
-""" Unit tests for function_to_image
+"""
+Unit tests for function_to_image.
 """
 
-import unittest
 import os
-import sys
-import tempfile
 import numpy as np
 
 from pet_imaging_tools.image_processing.function_to_image import function_to_image
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-# pylint: disable=protected-access, missing-docstring, invalid-name, line-too-long
-
-
-class Test_function_to_image(unittest.TestCase):
-
-  def test_function_to_image_empty(self):
-    with tempfile.TemporaryDirectory() as tmp:
-      out = os.path.join(tmp, 'test')
-      function_to_image(out, 'empty')
-      arr = np.fromfile(out + '.raw')
-      self.assertTrue(np.allclose(arr, 0.))
-
-
-if __name__ == '__main__':
-  suite = unittest.defaultTestLoader.loadTestsFromTestCase(
-      Test_function_to_image
-  )
-  testResult = unittest.TextTestRunner(verbosity=2).run(suite)
+def test_function_to_image_empty(tmp_path):
+  """
+  Test if an empty image is properly generated with a constant function f(x,y,z)=0.
+  """
+  out = os.path.join(tmp_path, 'test')
+  function_to_image(out, 'empty')
+  arr = np.fromfile(out + '.raw')
+  assert np.allclose(arr, 0.)
